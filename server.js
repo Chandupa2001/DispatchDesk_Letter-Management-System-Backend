0f4d9569d552ec2d35c0245d7afe4ed3cfc13570
Chandupa2001 <chanduparanawaka2001@gmail.com>
2025-01-08 08:55:26 +0530

    Connected all functions

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js';
 import letterRouter from './routes/letterRouter.js';
+import accountRouter from './routes/accountsRouter.js';
+import establishmentRouter from './routes/establishmentRouter.js';
+import surveyingRouter from './routes/surveyingRouter.js';
+import recordRoomRouter from './routes/recordRoomRouter.js';
 
 // app config
 const app = express();
@@ -17,7 +21,10 @@ connectDB();
 
 // api endpoints
 app.use("/api/letter", letterRouter)
-
+app.use("/api/accounts", accountRouter)
+app.use("/api/establishment", establishmentRouter)
+app.use("/api/surveying", surveyingRouter)
+app.use("/api/recordRoom", recordRoomRouter)
 
 app.listen(port, ()=>{
     console.log(`Server Started on http://localhost:${port}`)
