716fba44a5adc9365f6d801cc7367ae7462a14d3
Chandupa2001 <chanduparanawaka2001@gmail.com>
2025-01-26 23:59:52 +0530

    Added route for search query

--- routes/letterRouter.js
+++ routes/letterRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addLetter, approveLetter, fectchLetters } from '../controllers/letterController.js';
+import { addLetter, approveLetter, fectchLetters, searchLetters } from '../controllers/letterController.js';
 
 const letterRouter = express.Router();
 
 letterRouter.post("/add", addLetter);
 letterRouter.post("/approve", approveLetter);
 letterRouter.get("/get", fectchLetters);
+letterRouter.post("/search", searchLetters);
 
 export default letterRouter;
